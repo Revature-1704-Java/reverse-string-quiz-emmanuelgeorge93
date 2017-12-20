76ade59bd1dfe294fd4f585c2bec9ee528b43116
Emmanuel <emmanuelgeorge93@gmail.com>
2017-12-20 10:43:04 -0500

    my version

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -2,6 +2,15 @@ package quiz;
 
 public class ReverseString {
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+        //return new StringBuffer(input).reverse().toString();
+        char[] forwards = input.toCharArray();
+        //char[] backwards = input.toCharArray();
+        String backwards = "";
+        
+        for(int i = forwards.length-1; i >= 0; i--) {
+        	backwards += forwards[i];
+        }
+
+        return backwards;
     }
 }
